decdc777e1c9630aa78c44a738003e206dddaba2
alihaidergithub <vergefuture@gmail.com>
2023-05-04 17:34:12 +0500

    Initialization
    React Init

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Greeting(){
+  return <h2>My First Application</h2>
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Greeting />);
