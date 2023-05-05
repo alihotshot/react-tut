d7a4fbae23cd85edfcb57b1c2f00f71eac7b8dee
alihaidergithub <vergefuture@gmail.com>
2023-05-05 19:53:34 +0500

    BookList
    BookList Component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,29 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
-function Greeting() {
-  return <h2>Interface</h2> 
-}
+const BookList = () => {
+  return (
+    <section>
+      <Book />
+    </section>
+  )
+};
+
+const Book = () => {
+  return (
+    <article>
+      <Image />
+      <Title />
+      <Author />
+    </article>
+  );
+};
+
+const Image = () => <h2>Image Placeholder</h2>;
+const Title = () => <h2>Title of the Book</h2>;
+const Author = () => {
+  return <h4>Author</h4>;
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
-root.render(<Greeting />)
+root.render(<BookList />)
