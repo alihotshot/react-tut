87f155efb328ba66b19fd6e270c91a6c0e3a1323
alihaidergithub <vergefuture@gmail.com>
2023-05-09 19:05:54 +0500

    Till Children PROPS
    Till Children PROPS

--- src/index.js
+++ src/index.js
@@ -2,34 +2,64 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './style.css'
 
+const firstBook = {
+  img: './images/Book1.webp',
+  title: 'Power of Chemistry',
+  author: 'John Delta',
+}
+
+const secondBook = {
+  img: './images/Book2.webp',
+  title: 'Atomic Habit',
+  author: 'Elon Musk',
+}
+
 const BookList = () => {
   return (
     <section className="booklist">
-      <Book />
-      <Book />
-      <Book />
+      <Book
+        img={firstBook.img}
+        title={firstBook.title}
+        author={firstBook.author}
+      >
+        <p>
+          Think differently and find the courage to challenge the status quo
+          with this mindset-shifting guide here.
+        </p>
+      </Book>
+      <Book
+        img={secondBook.img}
+        title={secondBook.title}
+        author={secondBook.author}
+      />
     </section>
   )
 }
 
-const Book = () => {
+// Call properties as a special props & children props
+const Book = ({ img, title, author, children }) => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img src='{img}' alt='{title}' />
+      <h2>{title}</h2>
+      <h4>{author}</h4>
+      {children}
     </article>
   )
 }
 
-const Image = () => {
-  return <img src="./images/Book1.webp" alt="Power of Chemistry" />
-}
+// Access properties without dot in Props
 
-const Title = () => <h2>Power of Chemistry </h2>
-const Author = () => {
-  return <h4>John Delta</h4>
-}
+// const Book = (props) => {
+//   const { img, title, author } = props
+//   return (
+//     <article className="book">
+//       <img src={img} alt={title} />
+//       <h2>{title}</h2>
+//       <h4>{author}</h4>
+//     </article>
+//   )
+// }
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<BookList />)
